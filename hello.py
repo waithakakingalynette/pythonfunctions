d4c9c6a46c8e48cf3ef52978d6739e13dcac3fb6
waithakakingalynette <warukiralynette@gmail.com>
2023-03-31 19:54:59 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+def hello(name):
+    print(f"hello {name}")
+   
---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtraction(a,b):
+       answer=a-b
+       return answer
+
+def division(c,d):
+        answer=c//d
+        return answer
+
+def multiply(g,h):
+    answer=g*h
+    return answer
+
+def remainder(r,q):
+    answer=r%q
+    return answer   
+
+            
